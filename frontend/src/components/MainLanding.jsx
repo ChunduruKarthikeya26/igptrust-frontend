e2c0e076aff6594fbda1dd65da1880d4a3553a29
Karthikeya <karthikeyachunduru@gmail.com>
2026-06-22 15:50:30 +0530

    Added main landing blank page

---
+++ frontend/src/components/MainLanding.jsx
@@ -0,0 +1,14 @@
+
+
+const MainLanding = () => {
+  return (
+    <div>
+      <h1>Welcome to Our App</h1>
+      <p>This is the main landing page.</p>
+    </div>
+  );
+};
+
+export default MainLanding;
+
+// This is for landing main page
